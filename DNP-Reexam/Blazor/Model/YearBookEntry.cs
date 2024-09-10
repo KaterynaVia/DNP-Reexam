e39ca7194895fb752462e081b536853693bae82c
KaterynaVia <118535737+KaterynaVia@users.noreply.github.com>
2024-09-10 09:11:54 +0200

    Blazor part

--- DNP-Reexam/Blazor/Model/YearBookEntry.cs
+++ DNP-Reexam/Blazor/Model/YearBookEntry.cs
@@ -1,22 +1,30 @@
-﻿namespace Blazor.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Model;
 
 public class YearBookEntry
 {
+    [Required]
     public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
     public string Name { get; set; }
+    [MaxLength(100)]
     public string Pronouns { get; set; }
     public string FunFact { get; set; }
+    [Required]
     public string ImageUrl { get; set; }
-    public DateTime StartedYear { get; set; }
+    [Range(1900, 2100)]
+    public int Year { get; set; }
 
-    public YearBookEntry(int id, string name, string pronouns, string funFact, string imageUrl, DateTime startedYear)
+    public YearBookEntry(int id, string name, string pronouns, string funFact, string imageUrl, int year)
     {
         Id = id;
         Name = name;
         Pronouns = pronouns;
         FunFact = funFact;
         ImageUrl = imageUrl;
-        StartedYear = startedYear;
+        Year = year;
     }
     
 
