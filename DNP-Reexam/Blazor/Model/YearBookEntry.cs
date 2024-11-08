aee8ea37148e5933c7b2a8c0e3102b68ae7f4296
KaterynaVia <118535737+KaterynaVia@users.noreply.github.com>
2024-11-08 13:03:15 +0100

    blazor part with new url link

--- DNP-Reexam/Blazor/Model/YearBookEntry.cs
+++ DNP-Reexam/Blazor/Model/YearBookEntry.cs
@@ -1,24 +1,35 @@
-﻿namespace Blazor.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blazor.Model;
 
 public class YearBookEntry
 {
+    [Required]
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Pronouns { get; set; }
-    public string FunFact { get; set; }
-    public string ImageUrl { get; set; }
-    public DateTime StartedYear { get; set; }
 
-    public YearBookEntry(int id, string name, string pronouns, string funFact, string imageUrl, DateTime startedYear)
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+
+    [MaxLength(100)]
+    public string Pronouns { get; set; }
+
+    public string FunFact { get; set; }
+
+    [Required]
+    public string ImageUrl { get; set; }
+
+    [Required]
+    [Range(1900, 2100)]
+    public int Year { get; set; }
+
+    public YearBookEntry(int id, string name, string pronouns, string funFact, string imageUrl, int year)
     {
         Id = id;
         Name = name;
         Pronouns = pronouns;
         FunFact = funFact;
         ImageUrl = imageUrl;
-        StartedYear = startedYear;
+        Year = year;
     }
-    
-
-    
 }
--- DNP-Reexam/Blazor/Services/InMemoryYearBookService.cs
+++ DNP-Reexam/Blazor/Services/InMemoryYearBookService.cs
@@ -1,37 +1,36 @@
 ﻿using Blazor.Model;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Blazor.Services;
-
-public class InMemoryYearBookService:IYearBookService
+namespace Blazor.Services
 {
-    private List<YearBookEntry> _yearBookEntries;
-    private int _nextId = 1;
-
-    public InMemoryYearBookService()
+    public class InMemoryYearBookService : IYearBookService
     {
-        _yearBookEntries = new List<YearBookEntry>()
+        private readonly List<YearBookEntry> _yearBookEntries;
+        private int _nextId;
+
+        public InMemoryYearBookService()
         {
-            new YearBookEntry(1, "Harry", "he", "true", "image.jpg", DateTime.Now.AddDays(-1)),
-            new YearBookEntry(2, "Harry2", "he", "true", "image.jpg", DateTime.Now.AddDays(-1)),
-            new YearBookEntry(3, "Harry3", "he", "true", "image.jpg", DateTime.Now.AddDays(-1)),
-            new YearBookEntry(4, "Harry4", "he", "true", "image.jpg", DateTime.Now.AddDays(-1)),
-            new YearBookEntry(5, "Harry5", "he", "true", "image.jpg", DateTime.Now.AddDays(-1)),
-        };
-        _nextId = _yearBookEntries.Max(d => d.Id) + 1;
-    }
-   
+            _yearBookEntries = new List<YearBookEntry>
+            {
+                new YearBookEntry(1, "Harry", "he", "Loves hiking", "https://img.freepik.com/free-photo/man-glasses-orange-shirt-surprised-confused-standing-green-wall_141793-61019.jpg?size=626&ext=jpg", 2020),
+                new YearBookEntry(2, "Harry2", "he", "Fan of science fiction novels", "https://img.freepik.com/free-photo/man-glasses-orange-shirt-surprised-confused-standing-green-wall_141793-61019.jpg?size=626&ext=jpg", 2021),
+                new YearBookEntry(3, "Harry3", "he", "Enjoys painting", "https://img.freepik.com/free-photo/man-glasses-orange-shirt-surprised-confused-standing-green-wall_141793-61019.jpg?size=626&ext=jpg", 2010),
+                new YearBookEntry(4, "Harry4", "he", "Speaks 4 languages", "https://img.freepik.com/free-photo/man-glasses-orange-shirt-surprised-confused-standing-green-wall_141793-61019.jpg?size=626&ext=jpg", 2018),
+                new YearBookEntry(5, "Harry5", "he", "Likes dancing", "https://img.freepik.com/free-photo/medium-shot-woman-wearing-headphones_23-2149818230.jpg?t=st=1731065514~exp=1731069114~hmac=31d5a63c2da1db5f317216fd9ba28500f293b1a7ebb31445b1f0ae6ccf4f138e&w=1060", 2015)
+            };
+            _nextId = _yearBookEntries.Max(d => d.Id) + 1;
+        }
 
-    
+        public List<YearBookEntry> GetAllYearBookEntries()
+        {
+            return _yearBookEntries.OrderBy(d => d.Year).ToList();
+        }
 
-    public List<YearBookEntry> getAllYearBookEntries()
-    {
-        return _yearBookEntries.OrderBy(d => d.StartedYear).ToList();
-    }
-
-    public void AddYearBookEntry(YearBookEntry yearBookEntry)
-    {
-        yearBookEntry.Id = _nextId++;
-        yearBookEntry.StartedYear = DateTime.Now;
-        _yearBookEntries.Add(yearBookEntry);
+        public void AddYearBookEntry(YearBookEntry yearBookEntry)
+        {
+            yearBookEntry.Id = _nextId++;
+            _yearBookEntries.Add(yearBookEntry);
+        }
     }
 }
