251d3c9f28e6804fa5dcbdb0f5b83f0919b092f7
KaterynaVia <118535737+KaterynaVia@users.noreply.github.com>
2024-12-09 13:42:08 +0100

    Fixed view new year book entry page

--- DNP-Reexam/Blazor/Program.cs
+++ DNP-Reexam/Blazor/Program.cs
@@ -10,13 +10,16 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<IYearBookService, InMemoryYearBookService>();
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days.
+    // You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseHttpsRedirection();
